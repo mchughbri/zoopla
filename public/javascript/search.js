92ae1c6461b228ef571b778c751fe06a18cb5c05
Dan Jordan <djordan@salmon.com>
2018-08-08 14:09:31 +0100

    second effort

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,4 +1,5 @@
 const HeaderSearch = function() {
+    let errorTimeout = null;
     function init() {
         addEvents();        
     }
@@ -6,24 +7,35 @@ const HeaderSearch = function() {
         
         const searchForm = document.querySelector('.search-property');
         if(!searchForm){
-            return;
+            return false;
         }
         //submit listener
         searchForm.addEventListener('submit',submitSearch); 
         
     } 
-    function submitSearch(){
+    function submitSearch(evt){        
         const searchForm = document.querySelector('.search-property');
         const searchField = document.querySelector('.search-property__input');        
         if(!searchField.value){
             showError("This field cannot be empty");
+            evt.preventDefault();
             return false;
-        } else {
-            return true;
         }
     }
     function showError(message) {
-        alert(message);
+        const formErrors = document.querySelector('.search-property__form-errors');
+        if(!formErrors){
+            return false;
+        }
+        //populate and show error
+        formErrors.innerHTML = message;
+        formErrors.classList.remove("hidden");
+
+        //reset the hide timout
+        clearTimeout(errorTimeout);
+        errorTimeout = setTimeout(function(){
+            formErrors.classList.add("hidden");
+        },8000);
     }
     return {
         init : init
